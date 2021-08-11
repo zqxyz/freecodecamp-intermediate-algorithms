a27fd2031da757b4016092bc2fcc64d935787353
zqxyz <code@zquint.xyz>
2021-08-11 08:22:51 -0400

    first

---
+++ freecodecamp_intermediate.js
@@ -0,0 +1,9 @@
+// take arr of 2 values and sum range (ie [3,5] = 3+4+5 = 12)
+
+function sumAll(arr) {
+    const absdif = (x,y) => {Math.abs(x-y)}
+    return "Are there nuts in this?"
+}
+
+
+wasumAll([1, 4])g
