afa20ce592eeb7ef81bd50b93d0227bdd7cbf169
zqxyz <code@zquint.xyz>
2021-08-11 10:22:51 -0400

    added sumAll

--- freecodecamp_intermediate.js
+++ freecodecamp_intermediate.js
@@ -1,9 +1,28 @@
-// take arr of 2 values and sum range (ie [3,5] = 3+4+5 = 12)
+/**
+ * @author: code@zquint.xyz
+ * @description: freecodecamp.org intermediate alorithm solutions
+ */
 
+
+/**
+ * Take arr of 2 values and sum range (ie [3,5] = 3+4+5 = 12)
+ * @param {number} arr array of 2 integers
+ * @returns Sum of provided values and all numbers in between
+ */
 function sumAll(arr) {
-    const absdif = (x,y) => {Math.abs(x-y)}
-    return "Are there nuts in this?"
+    const start = arr[0]
+    const end = arr[1]
+
+    let sum = start
+    let x = start-end
+    while (x != 0) {
+        sum += (start - (x))
+        x > 0 ? x-- : x++
+    }
+
+    return sum
 }
 
-
-wasumAll([1, 4])
+console.log('sumAll [4, 12]: ' + sumAll([4, 12]))
+console.log('sumAll [5, 1]:  ' + sumAll([5, 1]))
+console.log('sumAll [8, 8]:  ' + sumAll([8, 8]))
