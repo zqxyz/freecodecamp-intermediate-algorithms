a373a8cc3cb417514cea161ae403ba7524825f21
zqxyz <code@zquint.xyz>
2021-08-18 14:55:38 -0400

    pig latin and string replace algs added

--- freecodecamp_intermediate.js
+++ freecodecamp_intermediate.js
@@ -1,6 +1,7 @@
 /**
  * @author: code@zquint.xyz
  * @description: freecodecamp.org intermediate algorithm solutions
+ * note: I am not naming these functions
  */
 
 
@@ -12,14 +13,12 @@
 function sumAll(arr) {
     const start = arr[0]
     const end = arr[1]
-
     let sum = start
     let x = start - end
     while (x != 0) {
         sum += (start - (x))
         x > 0 ? x-- : x++
     }
-
     return sum
 }
 // console.log(sumAll([8, 3]));
@@ -87,5 +86,53 @@ function whatIsInAName(collection, query) {
     })
     return arr
 }
-console.log(whatIsInAName([{ "apple": 1, "bat": 2 }, { "bat": 2 }, { "apple": 1, "bat": 2, "cookie": 2 }],
-    { "apple": 1, "bat": 2 }))
+// console.log(whatIsInAName([{ "apple": 1, "bat": 2 }, { "bat": 2 }, { "apple": 1, "bat": 2, "cookie": 2 }],
+//     { "apple": 1, "bat": 2 }))
+
+
+/**
+ * Converts string to spinal case string
+ * @param {string} str string to convert to spinal case
+ * @returns Spinal case string
+ */
+function spinalCase(str) {
+    str = str.replace(/_/g, '-')
+    str = str.replace(/([A-Z])/g, "-$1")
+    str = str.replace(/\W{1,}/g, '-')
+    str = str.replace(/^\W/g, '')
+    str = str.replace(/\W$/g, '')
+    str = str.toLowerCase()
+    return str;
+}
+// console.log(spinalCase("TheAndy_Griffith_Show-"))
+
+
+/**
+ * Converts a string to a pig latin string
+ * @param {string} str String to convert to pig latin
+ * @returns String converted to pig latin
+ */
+function translatePigLatin(str) {
+    if (!/[a|e|i|o|u]/.test(str)) str += 'ay'
+    else if (!/[a|e|i|o|u]/.test(str.charAt(0))) str = str.replace(/([^a|e|i|o|u]+)([\1-^]+)/gi, '$2$1ay')
+    else str += 'way'
+    return str
+}
+// console.log(translatePigLatin("paragraphs"))
+
+
+/**
+ * Find and replace in a string while preserving case of first letter
+ * @param {string} str String in which to find and replace based on 2nd and 3rd parameters
+ * @param {string} before Characters inside str to match for replacement
+ * @param {string} after Characters to replace 'before' param with
+ * @returns String with replaced characters
+ */
+ function myReplace(str, before, after) {
+    if (/[A-Z]/.test(before.charAt(0))) after = after.charAt(0).toUpperCase() + after.slice(1)
+    else after = after.toLowerCase()
+    str = str.replace(before, after)
+    return str;
+}
+
+console.log(myReplace("Let us go to the Store", "Store", "mall"))
