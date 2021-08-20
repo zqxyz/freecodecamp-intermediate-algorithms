ffafdd6d65734076e549b0c0bdd579c0b6944b6c
zach <code@zquint.xyz>
2021-08-20 06:34:27 -0400

    removed files

--- test.js
+++
@@ -1,7 +0,0 @@
-function tempButtonDisable() {
-    const btn = document.getElementById('estimateSubmit')
-    btn.setAttribute('disabled')
-    setTimeout(()=>btn.removeAttribute('disabled'), 3000)
-}
-
-tempButtonDisable()
